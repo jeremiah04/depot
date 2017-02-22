2057a61eccdd562347df194e81eda83296ac3fdb
Jeremiah Mall <jeremiahmall@yahoo.com>
2017-02-22 21:10:07 +0000

    hello

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,10 @@
 class LineItemsController < ApplicationController
-  include CurrentCart
-  before_action :set_cart, only: [:create]
-  
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+      
+      include CurrentCart
+     
+      before_action :set_cart, only: [:create]
+   
+   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
   # GET /line_items.json
@@ -27,12 +29,12 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    product = Product.find(params[:product_id])
-  @line_item = @cart.line_items.build(product: product)
+  product = Product.find(params[:product_id])
+  @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line Items'}
+       format.html { redirect_to @line_item.cart, notice: 'Product added to Cart!' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -60,7 +62,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url, notice: 'Product successfully removed from Cart!' }
       format.json { head :no_content }
     end
   end
@@ -73,6 +75,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'store#index'
+root to: 'store#index', as: 'store'
 end
