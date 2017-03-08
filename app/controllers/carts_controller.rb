9ceb0eb50a6839f84ff6cd96dc06fac90e315cd5
Jeremiah Mall <jeremiahmall@yahoo.com>
2017-03-08 20:52:31 +0000

    new

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
-
 class CartsController < ApplicationController
+  skip_before_action :authorize
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
@@ -78,7 +78,7 @@ class CartsController < ApplicationController
 
 
 def invalid_cart
-logger.error "Attempt to access invalid cart #{params[:id]}"
-redirect_to store_index_url, notice: 'Invalid cart'
-end    
+ logger.error "Attempt to access invalid cart #{params[:id]}"
+  redirect_to store_index_url, notice: 'Invalid cart'
 end
+end # this is the last end in the file
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  
+ get 'admin' => 'admin#index'
+ controller :sessions do
+ get 'login' => :new
+ post 'login' => :create
+ delete 'logout' => :destroy
+ end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
@@ -7,4 +17,5 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'store#index', as: 'store'
+
 end
